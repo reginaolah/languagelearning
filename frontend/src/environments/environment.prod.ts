6262c78bb2218c734c15b27d5dc5efcb44553fc3
Regina Olah <y3wiw6@inf.elte.hu>
2020-12-28 15:39:05 +0100

    added server.js

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true
 };
 
-//export const baseUrl: string = 'http://italkiclone.herokuapp.com';
-export const baseUrl: string = 'http://localhost:3000';
+export const baseUrl: string = 'http://languagelearningapp.herokuapp.com';
+//export const baseUrl: string = 'http://localhost:3000';
 
